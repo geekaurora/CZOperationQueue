c370ef0b46c61864a65c93114e0cdcdb01967695
Cheng Zhang <czhang@groupon.com>
2017-08-09 14:34:22 -0700

    clean up code

--- CZOperationQueueDemo/CZOperationQueueDemo/CZOperationQueue/GPOperationQueue.swift
+++ CZOperationQueueDemo/CZOperationQueueDemo/CZOperationQueue/GPOperationQueue.swift
@@ -7,46 +7,15 @@
 
 import UIKit
 
-extension Operation {
-    var canStart: Bool {
-        return !isCancelled && isReady && !isExecuting
-    }
-}
-
 open class GPOperationQueue: NSObject {
     var maxConcurrentOperationCount: Int
     fileprivate var operationsManager: CZOperationsManager
-    /** CZDispatchQueue */
-    /// Serial queue acting as gate keeper, to ensure only one thread is blocked
-    fileprivate let gateKeeperQueue: DispatchQueue
-    /// Actual concurrent working queue
     fileprivate let jobQueue: DispatchQueue
-    /// Semahore to limit the max concurrent executions in dispatch queue
-    fileprivate let semaphore: DispatchSemaphore
-    fileprivate enum QueueLabel: String {
-        case gateKeeper, job
-        func prefix(_ label: String) -> String {
-            return label + "." + self.rawValue
-        }
-    }    
-    
+
     required public init(maxConcurrentOperationCount: Int = config.maxConcurrentOperationCount) {
-         self.maxConcurrentOperationCount = maxConcurrentOperationCount
+        self.maxConcurrentOperationCount = maxConcurrentOperationCount
         operationsManager = CZOperationsManager(maxConcurrentOperationCount: maxConcurrentOperationCount)
-        
-        let label = config.label
-        let maxConcurrentCount = maxConcurrentOperationCount
-
-        /// Initialize semaphore
-        semaphore = DispatchSemaphore(value: maxConcurrentCount)
-        /// Serial queue acting as gate keeper, to ensure only one thread is blocked
-        gateKeeperQueue = DispatchQueue(label: QueueLabel.gateKeeper.prefix(label),
-                                        attributes: [])
-
-        /// Actual concurrent working queue
-        jobQueue = DispatchQueue(label: QueueLabel.job.prefix(label),
-                                 attributes:  [.concurrent]
-        )
+        jobQueue = DispatchQueue(label: config.label, attributes:  [.concurrent])
         super.init()
         operationsManager.delegate = self
     }
@@ -71,7 +40,7 @@ extension GPOperationQueue: CZOperationsManagerDelegate {
 
 fileprivate extension GPOperationQueue {
     fileprivate struct config {
-        static let maxConcurrentOperationCount = 128
+        static let maxConcurrentOperationCount: Int = .max
         static let label = "com.tony.underlyingQueue"
     }
 
@@ -84,7 +53,6 @@ fileprivate extension GPOperationQueue {
 
     func _runNextOperations() {
         while (!operationsManager.isEmpty && !operationsManager.reachedMaxConcurrentCount) {
-            //self.semaphore.wait()
             operationsManager.dequeueFirstReadyOp { (op, subqueue) in
                 if let op = op as? TestOperation {
                     print("dequeued op: \(op.jobIndex)")
@@ -92,7 +60,6 @@ fileprivate extension GPOperationQueue {
                 self.jobQueue.async {
                     if (op.canStart) {
                         op.start()
-                        //self.semaphore.signal()
                     }
                 }
             }
@@ -100,4 +67,8 @@ fileprivate extension GPOperationQueue {
     }
 }
 
-
+extension Operation {
+    var canStart: Bool {
+        return !isCancelled && isReady && !isExecuting
+    }
+}
