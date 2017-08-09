972ed4c0335733d2303ad3070c051d7069f5fe6e
Cheng Zhang <czhang@groupon.com>
2017-08-09 16:39:56 -0700

    write unit test

--- CZOperationQueueDemo/CZOperationQueueDemoTests/CZOperationQueueDemoTests.swift
+++ CZOperationQueueDemo/CZOperationQueueDemoTests/CZOperationQueueDemoTests.swift
@@ -10,27 +10,38 @@ import XCTest
 @testable import CZOperationQueueDemo
 
 class CZOperationQueueDemoTests: XCTestCase {
-    
+    fileprivate lazy var testDataManager = TestDataManager.shared
+    fileprivate var gpOperationQueue: GPOperationQueue?
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testDataManager.removeAll()
+        gpOperationQueue = GPOperationQueue()
+        gpOperationQueue?.maxConcurrentOperationCount = 3
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        gpOperationQueue = nil
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testBasic() {
+        let operations = (0...10).map {TestOperation($0, testDataManager: testDataManager)}
+        // Priority
+        operations[0].queuePriority = .veryLow
+        operations[1].queuePriority = .low
+        operations[6].queuePriority = .veryHigh
+        // Dependency
+        operations[8].addDependency(operations[0])
+        gpOperationQueue?.addOperations(operations, waitUntilFinished: true)
+
+        let index0 = testDataManager.index(of: 0)!
+        let index1 = testDataManager.index(of: 1)!
+        let index6 = testDataManager.index(of: 6)!
+        let index8 = testDataManager.index(of: 8)!
+        XCTAssertTrue(index6 < index0, "Task6 should complete before Task0, as priorityOfTask6(veryHigh) > priorityOfTask0(veryLow)")
+        XCTAssertTrue(index1 < index0, "Task1 should complete before Task0, as priorityOfTask1(low) > priorityOfTask0(veryLow)")
+        XCTAssertTrue(index0 < index8, "Task0 should complete before Task8, as Task8 dependes on Task0")
     }
     
 }
