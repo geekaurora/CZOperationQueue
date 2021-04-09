4e7af9b89850a0fb438db7ff00dd4b69c58d73b2
Cheng Zhang <showt2me@gmail.com>
2021-04-09 21:53:23 +0900

    testCancelAllOperations: passed.

--- Tests/CZOperationQueueTests/CZOperationQueueTests.swift
+++ Tests/CZOperationQueueTests/CZOperationQueueTests.swift
@@ -4,7 +4,6 @@ import CZTestUtils
 @testable import CZOperationQueue
 
 final class CZOperationQueueTests: XCTestCase {
-  
   private enum Constant {
     static let timeOut: TimeInterval = 30
     static let maxConcurrentOperationCount = 3
@@ -14,7 +13,7 @@ final class CZOperationQueueTests: XCTestCase {
   }
   private let dataManager = TestDataManager.shared
   private var czOperationQueue: CZOperationQueue!
-
+  
   override func setUp() {
     dataManager.removeAll()
     czOperationQueue = CZOperationQueue(maxConcurrentOperationCount: Constant.maxConcurrentOperationCount)
@@ -28,7 +27,7 @@ final class CZOperationQueueTests: XCTestCase {
       [operation],
       allOperationsFinished: {
         dbgPrint("dataManager: \(self.dataManager)")
-
+        
         // Verify `operation` has been executed.
         XCTAssertTrue(operation.isExecuted, "operation should have been executed.")
         // Fulfill the expectatation.
@@ -50,12 +49,12 @@ final class CZOperationQueueTests: XCTestCase {
         
         // Verify `operations` have been executed.
         XCTAssertTrue(!operations.contains { !$0.isExecuted }, "All operations should have been executed.")
-       
+        
         // Verify results of dataManager are correct.
         let expected = Set(MockData.testIndexesArray)
         let actual = Set(self.dataManager.results())
         XCTAssertEqual(expected, actual, "Results are incorrect! expected = \(expected), \nactual=\(actual)")
-       
+        
         // Fulfill the expectatation.
         expectation.fulfill()
       })
@@ -72,7 +71,7 @@ final class CZOperationQueueTests: XCTestCase {
     operations[1].queuePriority = .low
     operations[8].queuePriority = .high
     operations[6].queuePriority = .veryHigh
-
+    
     // Add operations.
     czOperationQueue.addOperations(
       operations,
@@ -81,12 +80,12 @@ final class CZOperationQueueTests: XCTestCase {
         
         // Verify `operations` have been executed.
         XCTAssertTrue(!operations.contains { !$0.isExecuted }, "All operations should have been executed.")
-       
+        
         // Verify results of dataManager are correct.
         let expected = Set(MockData.testIndexesArray)
         let actual = Set(self.dataManager.results())
         XCTAssertEqual(expected, actual, "Results are incorrect! expected = \(expected), \nactual=\(actual)")
-       
+        
         // Verify `operations` execution order - should correspond to priorities.
         // operations[0].queuePriority = .veryLow
         let resultIndexOfOperation0 = self.dataManager.index(of: 0)!
@@ -102,22 +101,22 @@ final class CZOperationQueueTests: XCTestCase {
         let resultIndexOfOperation8 = self.dataManager.index(of: 8)!
         // operations[6].queuePriority = .veryHigh
         let resultIndexOfOperation6 = self.dataManager.index(of: 6)!
-
+        
         // Verify priority: .veryLow < .low
         XCTAssertTrue(
           resultIndexOfOperation1 < resultIndexOfOperation0,
           "Incorrect executing order of priority! resultIndexOfOperation1 = \(resultIndexOfOperation1), \nresultIndexOfOperation0 = \(resultIndexOfOperation0)")
-
+        
         // Verify priority: .low < .normal
         XCTAssertTrue(
           resultIndexOfOperation7 < resultIndexOfOperation1,
           "Incorrect executing order of priority! resultIndexOfOperation7 = \(resultIndexOfOperation7), \nresultIndexOfOperation1 = \(resultIndexOfOperation1)")
-
+        
         // Verify priority: .normal < .high
         XCTAssertTrue(
           resultIndexOfOperation8 < resultIndexOfOperation7,
           "Incorrect executing order of priority! resultIndexOfOperation8 = \(resultIndexOfOperation8), \resultIndexOfOperation7 = \(resultIndexOfOperation7)")
-
+        
         // Verify priority: .high < .veryHigh
         XCTAssertTrue(
           resultIndexOfOperation6 < resultIndexOfOperation8,
@@ -143,7 +142,7 @@ final class CZOperationQueueTests: XCTestCase {
     operations[1].queuePriority = .low
     operations[8].queuePriority = .high
     operations[6].queuePriority = .veryHigh
-
+    
     // Add operations.
     czOperationQueue.addOperations(
       operations,
@@ -152,18 +151,19 @@ final class CZOperationQueueTests: XCTestCase {
         
         // Verify `operations` have been executed.
         XCTAssertTrue(!operations.contains { !$0.isExecuted }, "All operations should have been executed.")
-       
+        
         // Verify results of dataManager are correct.
         let expected = Set(MockData.testIndexesArray + [11])
         let actual = Set(self.dataManager.results())
         XCTAssertEqual(expected, actual, "Results are incorrect! expected = \(expected), \nactual=\(actual)")
-       
+        
         // Verify `operations` execution order - should correspond to priorities.
+        
         // operations[0].queuePriority = .veryLow
         let resultIndexOfOperation0 = self.dataManager.index(of: 0)!
         // operations[11].queuePriority = .veryHigh
         let resultIndexOfOperation11 = self.dataManager.index(of: 11)!
-
+        
         // Verify priority: .veryLow < .veryHigh
         XCTAssertTrue(
           resultIndexOfOperation11 < resultIndexOfOperation0,
@@ -195,7 +195,7 @@ final class CZOperationQueueTests: XCTestCase {
     
     // Set operations[0] as operations[8]'s dependency.
     operations[8].addDependency(operations[0])
-
+    
     // Add operations.
     czOperationQueue.addOperations(
       operations,
@@ -204,12 +204,12 @@ final class CZOperationQueueTests: XCTestCase {
         
         // Verify `operations` have been executed.
         XCTAssertTrue(!operations.contains { !$0.isExecuted }, "All operations should have been executed.")
-       
+        
         // Verify results of dataManager are correct.
         let expected = Set(MockData.testIndexesArray)
         let actual = Set(self.dataManager.results())
         XCTAssertEqual(expected, actual, "Results are incorrect! expected = \(expected), \nactual=\(actual)")
-       
+        
         // Verify dependency - operations[0] is operations[8]'s dependency.
         // operations[8] should execute after operations[0].
         let resultIndexOfOperation0 = self.dataManager.index(of: 0)!
@@ -226,36 +226,31 @@ final class CZOperationQueueTests: XCTestCase {
     waitForExpectatation()
   }
   
-  func testAddOperationWithDependency2() {
+  func testCancelAllOperations() {
     let (waitForExpectatation, expectation) = CZTestUtils.waitWithInterval(Constant.timeOut, testCase: self)
     
+    // Add operations.
     let operations = MockData.testIndexesArray.map { TestOperation($0, dataManager: dataManager) }
-    operations[0].queuePriority = .veryLow
-    operations[1].queuePriority = .low
-    operations[6].queuePriority = .veryHigh
-    operations[8].addDependency(operations[0])
-    
     czOperationQueue.addOperations(
       operations,
       allOperationsFinished: {
+        dbgPrint("dataManager: \(self.dataManager)")
+        
+        // Verify `operations` shound't have been all executed.
+        let totalOperationsCount = MockData.testIndexesArray.count
+        let executedOperationsCount = self.dataManager.results().count
+        XCTAssertTrue(
+          executedOperationsCount < totalOperationsCount,
+          "operations shound't have been all executed. executedOperationsCount = \(executedOperationsCount), \ntotalOperationsCount = \(totalOperationsCount)")
+        
         // Fulfill the expectatation.
         expectation.fulfill()
-        
       })
     
-    //        DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + 1) {[weak self] in
-    //            guard let `self` = self else { return }
-    //            self.czOperationQueue?.cancelAllOperations()
-    //        }
-    
-    //        DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + 3) {[weak self] in
-    //            guard let `self` = self else { return }
-    //            let operation = TestOperation(11, dataManager: self.dataManager)
-    //            operation.queuePriority = .veryHigh
-    //            self.czOperationQueue?.addOperation(operation)
-    //        }
-    
-    dbgPrint("dataManager: \(dataManager)")
+    // Call `cancelAllOperations()` after 1 sec.
+    DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + 1) {
+      self.czOperationQueue.cancelAllOperations()
+    }
     
     // Wait for expectatation.
     waitForExpectatation()
