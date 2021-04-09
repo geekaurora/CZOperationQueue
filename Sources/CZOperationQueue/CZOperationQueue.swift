085fb7a7082cd627f158f5764e2f08ba91038985
Cheng Zhang <showt2me@gmail.com>
2021-04-09 16:19:29 +0900

    [Major] Fix build.

--- Sources/CZOperationQueue/CZOperationQueue.swift
+++
@@ -1,3 +0,0 @@
-struct CZOperationQueue {
-    var text = "Hello, World!"
-}
--- Tests/CZOperationQueueTests/CZOperationQueueTests.swift
+++ Tests/CZOperationQueueTests/CZOperationQueueTests.swift
@@ -6,7 +6,7 @@ final class CZOperationQueueTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CZOperationQueue().text, "Hello, World!")
+        // XCTAssertEqual(CZOperationQueue().text, "Hello, World!")
     }
 
     static var allTests = [
