5ce2a5f972b837d79f899c25080cb7b5afa2f2d0
Cheng Zhang <showt2me@gmail.com>
2021-04-09 18:42:17 +0900

    Comments.

--- Sources/CZOperationQueue/CZOperationQueue.swift
+++ Sources/CZOperationQueue/CZOperationQueue.swift
@@ -47,13 +47,13 @@ open class CZOperationQueue: NSObject {
   }
   
   /// Adds the specified `operation` to the operation queue.
-  /// It will execute when it reaches the queue head based on its priority and is ready / has no unfinished dependencies,
+  /// `operation` will execute when it reaches the queue head based on its priority / readiness / dependencies.
   open func addOperation(_ operation: Operation) {
     _addOperation(operation)
   }
   
   /// Adds the specified `operations` to the operation queue.
-  /// Eash operation will execute when it reaches the queue head based on its priority and is ready / has no unfinished dependencies.
+  /// Eash operation will execute when it reaches the queue head based on its priority / readiness / dependencies.
   ///
   /// - Parameters:
   ///   - operations: operations to be executed.
